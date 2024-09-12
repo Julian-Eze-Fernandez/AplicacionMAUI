928d560a39e88274b006313adcf2f05b1da66ccc
Usuario <Usuario@EQUIPO>
2024-09-12 12:52:52 -0300

    Agregar archivos de proyecto.

---
+++ AppPruebaMAUI/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPruebaMAUI
+{
+    public class Producto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageFilePath { get; set; } // Usar la ruta del archivo
+    }
+}
